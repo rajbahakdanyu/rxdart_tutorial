7000b1dd59735abd9b1dde5285cbd306fae2a9bc
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-23 13:33:28 +0545

    Added rxdart dependency

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RxDart Tutorial',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
