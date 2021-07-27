d4bdf0230be2b368a59007b6d0fefdaf059074fa
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-27 22:10:47 +0545

    Added reader screen

---
+++ lib/screens/reader.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Reader extends StatelessWidget {
+  const Reader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
