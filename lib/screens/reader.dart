e261e56637d4e9acbfed46c88f84fbffecef8778
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-28 12:52:33 +0545

    Added tab body

--- lib/screens/reader.dart
+++ lib/screens/reader.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Reader extends StatelessWidget {
   const Reader({Key? key}) : super(key: key);
@@ -22,20 +23,39 @@ class Reader extends StatelessWidget {
         body: TabBarView(
           children: [
             Scaffold(
-              body: Text('1'),
+              body: tabBody('Standard Dart Stream Controller.'),
             ),
             Scaffold(
-              body: Text('1'),
+              body: tabBody(
+                  'Exactly like a normal broadcast StreamController with one exception: this class is both a Stream and Sink.'),
             ),
             Scaffold(
-              body: Text('1'),
+              body: tabBody(
+                  'A special StreamController that captures the latest item that has been added to the controller, and emits that as the first item to any new listener.'),
             ),
             Scaffold(
-              body: Text('1'),
+              body: tabBody(
+                  'A special StreamController that captures all of the items that have been added to the controller, and emits those as the first items to any new listener.'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  tabBody(String desc) {
+    return ListView(
+      children: [
+        Text(desc),
+        SizedBox(height: 20.0),
+        Divider(),
+        Text(
+          'My Sonnet Text',
+          style: GoogleFonts.imFellEnglish(
+            textStyle: TextStyle(fontSize: 22.0),
+          ),
+        ),
+      ],
     );
   }
 }
